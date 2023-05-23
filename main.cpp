daddd44eb3258f92a79569e1d1bee8540dbb708e
幸丘 叶内 <220209@jc-21.jp>
2023-05-23 10:47:26 +0900

    windows programは完成

--- main.cpp
+++ main.cpp
@@ -1,6 +1,12 @@
 //インクルード
 #include <Windows.h>
 
+//定数宣言
+const char* WIN_CLASS_NAME = "SampleGame";  //ウィンドウクラス名
+const char* TITLE_NAME = "サンプルゲーム";  //タイトル名
+const int WINDOW_WIDTH = 800;  //ウィンドウの幅
+const int WINDOW_HEIGHT = 600; //ウィンドウの高さ
+
 //プロトタイプ宣言
 LRESULT CALLBACK WndProc(HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam);
 
@@ -11,7 +17,7 @@ int APIENTRY WinMain(HINSTANCE hInstance, HINSTANCE hPrevInst, LPSTR lpCmdLine, 
     WNDCLASSEX wc;
     wc.cbSize = sizeof(WNDCLASSEX);             //この構造体のサイズ
     wc.hInstance = hInstance;                   //インスタンスハンドル
-    wc.lpszClassName = "SampleGame";            //ウィンドウクラス名
+    wc.lpszClassName = WIN_CLASS_NAME;            //ウィンドウクラス名
     wc.lpfnWndProc = WndProc;                   //ウィンドウプロシージャ
     wc.style = CS_VREDRAW | CS_HREDRAW;         //スタイル（デフォルト）
     wc.hIcon = LoadIcon(NULL, IDI_APPLICATION); //アイコン
@@ -23,18 +29,22 @@ int APIENTRY WinMain(HINSTANCE hInstance, HINSTANCE hPrevInst, LPSTR lpCmdLine, 
     wc.hbrBackground = (HBRUSH)GetStockObject(GRAY_BRUSH); //背景（白）
     RegisterClassEx(&wc);  //クラスを登録
 
+    //ウィンドウサイズの計算
+    RECT winRect = { 0, 0, WINDOW_WIDTH, WINDOW_HEIGHT };
+    AdjustWindowRect(&winRect, WS_OVERLAPPEDWINDOW, FALSE); //表示スタイルは統一
+    int winW = winRect.right - winRect.left;     //ウィンドウ幅
+    int winH = winRect.bottom - winRect.top;     //ウィンドウ高さ
+
     //ウィンドウを作成
     HWND hWnd = CreateWindow(
-        "SampleGame",         //ウィンドウクラス名
-        "サンプルゲーム",     //タイトルバーに表示する内容
+        WIN_CLASS_NAME,         //ウィンドウクラス名
+        TITLE_NAME,     //タイトルバーに表示する内容
         WS_OVERLAPPEDWINDOW,
        // WS_OVERLAPPEDWINDOW | WS_VISIBLE,//スタイル（普通のウィンドウ） //|VISIBLE,を追加することで最初から表示できる。
-        //CW_USEDEFAULT,       //表示位置左（おまかせ）
-        //CW_USEDEFAULT,       //表示位置上（おまかせ）
-        0,
-        0,
-        800,                 //ウィンドウ幅
-        600,                 //ウィンドウ高さ
+        CW_USEDEFAULT,       //表示位置左（おまかせ）
+        CW_USEDEFAULT,       //表示位置上（おまかせ）
+        winW,                 //ウィンドウ幅
+        winH,                 //ウィンドウ高さ
         NULL,                //親ウインドウ（なし）
         NULL,                //メニュー（なし）
         hInstance,           //インスタンス
