398a39502d3bb1851b5f63eb0128f4d28e727c72
幸丘 叶内 <220209@jc-21.jp>
2023-05-26 10:24:22 +0900

    一応こっちでもプッシュ

--- Direct3D.h
+++ Direct3D.h
@@ -5,14 +5,13 @@
 //リンカ
 #pragma comment(lib, "d3d11.lib")
 
-//定数宣言
-const int WINDOW_WIDTH = 800;  //ウィンドウの幅
-const int WINDOW_HEIGHT = 600; //ウィンドウの高さ
+
 
 namespace Direct3D
 
 {
 	//初期化
+	//winW ウィンドウ幅 winH ウィンドウ高さ
 	void Initialize(int winW, int winH, HWND hWnd);
 
 
