4af0fe03192af3cdb79f7a29592c020b0f0781a4
幸丘 叶内 <220209@jc-21.jp>
2023-06-06 16:08:55 +0900

    とりあえずここまで

---
+++ Texture.cpp
@@ -0,0 +1,18 @@
+#include "Texture.h"
+
+Texture::Texture()
+{
+}
+
+Texture::~Texture()
+{
+}
+
+HRESULT Texture::Load(string fileName)
+{
+    return E_NOTIMPL;
+}
+
+void Texture::Release()
+{
+}
