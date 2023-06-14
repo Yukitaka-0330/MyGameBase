74d4ecd2411116f550e65890dcb3dba122be047b
幸丘 叶内 <220209@jc-21.jp>
2023-06-14 09:16:36 +0900

    おけ

--- Direct3D.cpp
+++ Direct3D.cpp
@@ -146,7 +146,7 @@ HRESULT Direct3D::InitShader()
         { "TEXCOORD", 0, DXGI_FORMAT_R32G32_FLOAT, 0, sizeof(XMVECTOR) , D3D11_INPUT_PER_VERTEX_DATA, 0 },//UVÀ•W
         { "NORMAL",	0, DXGI_FORMAT_R32G32B32_FLOAT, 0, sizeof(XMVECTOR) * 2 ,	D3D11_INPUT_PER_VERTEX_DATA, 0 },//–@ü
     };
-    hr = pDevice_->CreateInputLayout(layout, 2, pCompileVS->GetBufferPointer(), pCompileVS->GetBufferSize(), &pVertexLayout_);
+    hr = pDevice_->CreateInputLayout(layout, 3, pCompileVS->GetBufferPointer(), pCompileVS->GetBufferSize(), &pVertexLayout_);
     SAFE_RELEASE(pCompileVS);
     //pCompileVS->Release();
     if (FAILED(hr))
